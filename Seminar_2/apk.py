8ff8c12371b81859e862a74422f08c4239c92ed6
Your NameVladimirs77nt <vladimirs77@yandex.ru>
2024-01-28 17:23:58 +0500

    семинар 2

--- Seminar_2/apk.py
+++ Seminar_2/apk.py
@@ -1,7 +1,7 @@
 # СЕМИНАР №1
 # -----------------------
 
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
 
 app = Flask(__name__)
@@ -13,7 +13,44 @@ app = Flask(__name__)
 # приветствием пользователя по имени.
 
 @app.route("/")
-@app.route('/main/')
-def main():
-    context = {'title': 'Главная'}
-    return render_template('main.html', **context)
+@app.route('/page-1/')
+def page_1():
+    context = {'title': 'Страница 1'}
+    return render_template('page-1.html', **context)
+
+@app.route('/page-2/')
+def page_2():
+    context = {'title': 'Страница 2'}
+    return render_template('page-2.html', **context)
+
+@app.get('/page-3/')
+def load_image():
+    context = {'title': 'Загрузка изображения'}
+    return render_template('page-3.html', **context)
+
+@app.post('/page-3/')
+def upload():
+    if request.method == "POST":
+        f = request.files["file"]
+        print (f)
+    return 'Изображение загружено!'
+    # context = {'title': 'Изображение загружено!'}
+    # return render_template('page-3.html', **context)
+
+@app.get('/page-4/')
+def auth():
+    context = {'title': 'Авторизация'}
+    return render_template('page-4.html', **context)
+
+@app.post('/page-4/')
+def auth_true():
+    if request.method == "POST":
+        f = request.files["file"]
+        print (f)
+    return 'Изображение загружено!'
+    # context = {'title': 'Изображение загружено!'}
+    # return render_template('page-3.html', **context)
+
+# -----------------------
+if __name__ == "__main__":
+    app.run()
