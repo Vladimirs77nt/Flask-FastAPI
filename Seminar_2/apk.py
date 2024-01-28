751fb5ccb764a6f84fe948352b77a398e77e23ff
Your NameVladimirs77nt <vladimirs77@yandex.ru>
2024-01-28 19:14:32 +0500

    семинар 2

--- Seminar_2/apk.py
+++ Seminar_2/apk.py
@@ -23,6 +23,12 @@ def page_2():
     context = {'title': 'Страница 2'}
     return render_template('page-2.html', **context)
 
+
+# -----------------------
+# Задание №2
+# Создать страницу, на которой будет изображение и ссылка на другую страницу,
+# на которой будет отображаться форма для загрузки изображений.
+
 @app.get('/page-3/')
 def load_image():
     context = {'title': 'Загрузка изображения'}
@@ -37,19 +43,34 @@ def upload():
     # context = {'title': 'Изображение загружено!'}
     # return render_template('page-3.html', **context)
 
-@app.get('/page-4/')
+
+# -----------------------
+# Задание №3
+# Создать страницу, на которой будет форма для ввода логина и пароля.
+# При нажатии на кнопку "Отправить" будет произведена проверка соответствия логина
+# и пароля и переход на страницу приветствия пользователя или страницу с ошибкой.
+
+@app.route('/login/', methods=["POST", "GET"])
 def auth():
+    if request.method == "POST":
+        user_name = request.form.get["login"]
+        user_pass = request.form.get["password"]
+        if user_name == "admin" & user_pass == "qwerty":
+            return render_template('page-4-succ.html', **context)
+        return render_template('page-4-error.html', **context)
     context = {'title': 'Авторизация'}
     return render_template('page-4.html', **context)
 
-@app.post('/page-4/')
+@app.post('/user/')
 def auth_true():
-    if request.method == "POST":
-        f = request.files["file"]
-        print (f)
-    return 'Изображение загружено!'
-    # context = {'title': 'Изображение загружено!'}
-    # return render_template('page-3.html', **context)
+    context = {'title': 'Успешная авторизация'}
+    return render_template('page-4-succ.html', **context)
+
+@app.post('/user_error/')
+def auth_false():
+    context = {'title': 'Нет авторизации!!!'}
+    return render_template('page-4-error.html', **context)
+
 
 # -----------------------
 if __name__ == "__main__":
